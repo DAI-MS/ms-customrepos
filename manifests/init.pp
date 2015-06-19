0112705f4b0b92a3a852f9a243dc08c6dde71e5e
jvirdee <jvirdee@hilmers.daiad.dai.co.uk>
2015-06-19 14:26:30 +0100

    changed to add multiple repos - JV

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,18 +5,20 @@
 # Copyright 2015 Your name here, unless otherwise noted.
 #
 class customrepos {
-	file { '/etc/yum.repos.d':
+$repos = [ 'test1.repo', 'test2.repo', 'test3.repo', ]
+
+	file { "/etc/yum.repos.d":
 		ensure => directory,
 		mode => '0755',
 		owner => 'root',
 		group => 'root',
 	}
 	
-	file { '/etc/yum.repos.d/test.repo':
+	file { "/etc/yum.repos.d/$repos":
 		ensure => present,
 		mode => '0644',
 		owner => 'root',
 		group => 'root',
-		source => "puppet:///extra_files/test.repo"
+		source => "puppet:///extra_files/$repos"
 	}	
 }
